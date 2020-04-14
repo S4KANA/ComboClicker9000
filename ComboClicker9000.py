6829e2e78599658675626d56a5d63ab724e02983
S4KANA <sakanafurai@gmail.com>
2020-04-14 06:56:57 +0200

    Some fixes and changed hotkeys

--- ComboClicker9000.py
+++ ComboClicker9000.py
@@ -2,17 +2,20 @@ import pyautogui
 import pyWinhook
 import pythoncom
 import time
+from random import randrange
 
 def OnKeyboardEvent(event):
-    if int(event.Ascii) == 120: #wx
-        global exit
+    global first_pos
+    global second_pos
+
+    if (event.Key) == "Numpad0":
         print("[-] Exit button pressed")
-        exit = True
+        first_pos = (-1, -1)
+        second_pos = (-1, -1)
+        guide()
+        pyautogui.keyUp('w')
 
-    if int(event.Ascii) == 32: # space bar
-        global first_pos
-        global second_pos
-
+    if (event.Key) == "Numpad1": # space bar
         if first_pos[0] == -1:
             first_pos = pyautogui.position()
             print("[+] First position set")
@@ -23,9 +26,9 @@ def OnKeyboardEvent(event):
 
 def guide():
     print("")
-    print("1) Put your cursor on the combo creature and press the 'Space bar' key")
+    print("1) Put your cursor on the combo creature and press the 'Numpad 1' key")
     print("2) Click the creature and move your cursor to the wability box, then press the 'space bar' key again")
-    print("3) Press 'x' to stop the clicking (may have to press it multiple times)")
+    print("3) Press 'Numpad 0' to stop the clicking (may have to press it multiple times)")
 
 
 # globals
@@ -45,17 +48,15 @@ def main():
         global second_pos
         global exit
         while first_pos[0] != -1 and second_pos[0] != -1:
-            if exit:
-                first_pos = (-1, -1)
-                second_pos = (-1, -1)
-                guide()
-                exit = False
-                break
             pythoncom.PumpWaitingMessages()
-            pyautogui.press('w') # auto mana hotkey
-            pyautogui.click(first_pos[0], first_pos[1])
+            pyautogui.keyDown('w')
+            next_pos = (first_pos[0], first_pos[1])
+            if(next_pos[0] != -1):
+                pyautogui.click(next_pos[0], next_pos[1])
             time.sleep(0.1)
-            pyautogui.click(second_pos[0], second_pos[1])
+            next_pos = (second_pos[0], second_pos[1])
+            if (next_pos[0] != -1):
+                pyautogui.click(next_pos[0], next_pos[1])
 
 if __name__ == "__main__":
     main()
