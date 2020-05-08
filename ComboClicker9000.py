d004e620bf0503eefa96f8f177e509ecb0498c3e
S4KANA <sakanafurai@gmail.com>
2020-05-08 04:15:03 +0200

    Made it so the user chooses which keys to use

--- ComboClicker9000.py
+++ ComboClicker9000.py
@@ -7,39 +7,66 @@ from random import randrange
 def OnKeyboardEvent(event):
     global first_pos
     global second_pos
+    global position_key
+    global stop_key
 
-    if (event.Key) == "Numpad0":
-        print("[-] Exit button pressed")
-        first_pos = (-1, -1)
-        second_pos = (-1, -1)
-        guide()
-        pyautogui.keyUp('w')
+    if position_key == "":
+        position_key = event.Key
+        print("'" + position_key + "'" + " set as position selection key")
 
-    if (event.Key) == "Numpad1": # space bar
-        if first_pos[0] == -1:
-            first_pos = pyautogui.position()
-            print("[+] First position set")
-        elif second_pos[0] == -1:
-            second_pos = pyautogui.position()
-            print("[+] Second position set")
+    elif stop_key == "":
+        stop_key = event.Key
+        print("'" + stop_key + "'" + " set as stop key")
+
+    else:
+        if (event.Key) == stop_key:
+            print("[-] stop key pressed")
+            first_pos = (-1, -1)
+            second_pos = (-1, -1)
+            guide()
+            pyautogui.keyUp('w')
+
+        if (event.Key) == position_key:
+            if first_pos[0] == -1:
+                first_pos = pyautogui.position()
+                print("[+] First position set")
+            elif second_pos[0] == -1:
+                second_pos = pyautogui.position()
+                print("[+] Second position set")
+
     return True
 
 def guide():
-    print("")
-    print("1) Put your cursor on the combo creature and press the 'Numpad 1' key")
-    print("2) Click the creature and move your cursor to the wability box, then press the 'space bar' key again")
-    print("3) Press 'Numpad 0' to stop the clicking (may have to press it multiple times)")
+    global position_key
+    global stop_key
+    print("\n")
+    print("1) Put your cursor on the combo creature and press the '" + position_key + "' key")
+    print("2) Click the creature and move your cursor to the wability box, then press the '" + position_key + "' key again")
+    print("3) Press the '"+ stop_key +"' to stop the clicking (may have to press it multiple times)")
 
 
 # globals
 first_pos = (-1, -1)
 second_pos = (-1, -1)
+position_key = ""
+stop_key = ""
+
 exit = False
 
 def main():
     hm = pyWinhook.HookManager()
     hm.KeyDown = OnKeyboardEvent
     hm.HookKeyboard()
+
+
+    print("Choose key that selects mouse position")
+    while(position_key == ""):
+        pythoncom.PumpWaitingMessages()
+
+    print("\nChoose stop key")
+    while (stop_key == ""):
+        pythoncom.PumpWaitingMessages()
+
     guide()
 
     while True:
--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 
 
-Just a simple program for clicking through the Devoted Druid or Heliod/Spike Feeder combos on MTGO
+Just a simple program for clicking through (parts of) the Devoted Druid or Heliod/Spike Feeder combos on MTGO
+First the program will prompt you about choosing a position key and a stop key. 
 
 For Devoted Druid/Vizier: (Assumes "Auto-Mana" hotkey is the default 'w')
 
-1) Put your cursor on the MIDDLE of the Devoted Druid and press 'Numpad 1'
-2) Put your cursor in the BOTTOM RIGHT corner of the Devoted Druid and press 'Numpad 1'
-3) The program clicks through the combo indefinitely. Press 'Numpad 0' to make the program stop clicking (may need to press it multiple times)
+1) Put your cursor on the MIDDLE of the Devoted Druid and press the chosen position key
+2) Put your cursor in the BOTTOM RIGHT corner of the Devoted Druid and press the position key again
+3) The program clicks through the combo indefinitely. Press the chosen stop key to make the program stop clicking (may need to press it multiple times)
 
 For Heliod/Spike Feeder:
 
 1) Do the combo manually once and set up "Save targets" and auto yield to Heliod's triggered ability
-2) Put your cursor on the Spike Feeder and press 'Numpad 1'
+2) Put your cursor on the Spike Feeder and press the position key
 3) Click the Spike Feeder
-4) Put your cursor on the "You gain 2 life" option and press 'Numpad 1'
-5) The program clicks through the combo indefinitely. Press 'Numpad 0' to make the program stop clicking (may need to press it multiple times)
-
-
-Can also be used to pay for and shoot with Ballista.
+4) Put your cursor on the "You gain 2 life" option and press the position key
+5) The program clicks through the combo indefinitely. Press the stop key to make the program stop clicking
 
